4d933228e8ca9ab3484550e79b54d240940d25f7
psamora <psamora@outlook.com>
2014-08-18 22:13:12 -0400

    Project creation; added start of database info

---
+++ src/players/Database.java
@@ -0,0 +1,22 @@
+package players;
+
+import java.sql.*;
+
+public class Database {
+
+	public static void main(String[] args) {
+		  String url = "jdbc:mysql://localhost:3306/";
+		  String dbName = "demo”
+		  String driver = "com.mysql.jdbc.Driver";
+		  String userName = "root"; 
+		  String password = "mypasswd";
+		  try {
+		  Class.forName(driver).newInstance();
+		  Connection conn = DriverManager.getConnection(url+dbName,userName,password);
+		  
+		  conn.close();
+		  } catch (Exception e) {
+		  e.printStackTrace();
+		  }
+		  }
+}
